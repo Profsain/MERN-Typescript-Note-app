b25121aa0d4a031b414ea1d60b3c52e116b56d69
profsain <husseinimudiking@gmail.com>
2023-12-03 10:38:31 +0100

    add router

---
+++ server/src/routes/users.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import * as UsersController from '../controllers/users';
+import { requiresAuth } from '../middleware/auth';
+
+const router = express.Router();
+
+router.get('/me', requiresAuth, UsersController.getAuthenticatedUser);
+
+router.post('/signup', UsersController.signup);
+
+router.post('/login', UsersController.login);
+
+router.post('/logout', UsersController.logout);
+
+export default router;
